b4b7f8feb7c014d60095bb37783e98502d75b52c
kpam92 <kpam1992@gmail.com>
2017-07-07 13:37:47 -0700

    card migration

--- rails_lib/db/migrate/20170707203622_create_cards.rb
+++ rails_lib/db/migrate/20170707203622_create_cards.rb
@@ -1,7 +1,8 @@
 class CreateCards < ActiveRecord::Migration
   def change
     create_table :cards do |t|
-
+      t.string :front, null: false
+      t.string :back, null: false
       t.timestamps null: false
     end
   end
