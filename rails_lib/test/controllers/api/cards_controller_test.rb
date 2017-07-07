10627c8e551037bc6429433fda562ea89a71e44f
kpam92 <kpam1992@gmail.com>
2017-07-07 14:31:47 -0700

    fix

--- rails_lib/test/controllers/api/cards_controller_test.rb
+++ rails_lib/test/controllers/api/cards_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class CardsControllerTest < ActionController::TestCase
+class Api::CardsControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
