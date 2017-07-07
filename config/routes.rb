259ec395e64d27e1748a2b4c81582ae9c11fa963
kpam92 <kpam1992@gmail.com>
2017-07-07 16:26:53 -0700

    jbuilder for api call

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  namespace :api do
+
+  namespace :api, defaults: { format: :json } do
     resources :cards, only: [:index]
   end
 
-  root to: redirect("/api/cards")
+  # root to: redirect("/api/cards")
 end
