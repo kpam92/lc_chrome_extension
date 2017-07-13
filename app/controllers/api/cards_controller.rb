4a110367a07cf0c2bf5cb8d0a60cce62fbf45fb9
kpam92 <kpam1992@gmail.com>
2017-07-12 23:11:57 -0700

    working on nokogiri

--- app/controllers/api/cards_controller.rb
+++ app/controllers/api/cards_controller.rb
@@ -1,6 +1,20 @@
+require 'open-uri'
+require 'byebug'
 class Api::CardsController < ApplicationController
 
   def index
+    doc = Nokogiri::HTML(open("https://leetcode.com/notes/"))
+    byebug
+    array_fronts = doc.xpath("//h4[@class='panel-title']")
+    array_backs = doc.xpath("//div[@class='note-content]/p")
     @cards = Card.all
+
+    # array_fronts.each_with_index do |front,idx|
+    #   front = front
+    #   back = array_backs[idx]
+    #   new_card= Card.new(front,back)
+    #   new_card.save
+    # end
+
   end
 end
