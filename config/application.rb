4ced4a9d28df23c7a564d6e9c35af1685999acc1
kpam92 <kpam1992@gmail.com>
2017-07-12 19:01:27 -0700

    rack cors

--- config/application.rb
+++ config/application.rb
@@ -23,10 +23,10 @@ module LC_chrome_extension
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
-    config.middleware.insert_before 0, "Rack::Cors" do
+    config.middleware.use "Rack::Cors" do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
+        resource '*', :headers => :any, :methods => [:get, :post, :delete, :put, :options]
       end
     end
   end
