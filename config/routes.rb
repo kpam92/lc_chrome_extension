ef4d0f3b8cf4cc0222dec152353a8a815b18c47d
kpam92 <kpam1992@gmail.com>
2017-07-13 22:49:11 -0700

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json }, path: '/api' do
-    resources :cards, only: [:index]
+    resources :cards, only: [:index,:show,:create]
+    resources :users, only: [:index,:show,:create]
   end
 
   root "static_pages#root"
